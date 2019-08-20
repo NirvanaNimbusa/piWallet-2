5b3ba3052f996dff02a3f0f66070c22c8901fbee
Marquis Ogre <marquisogre@gmail.com>
2019-08-20 12:35:06 +0530

    Initial Commit

---
+++ settings.php
@@ -0,0 +1,27 @@
+<?php
+
+$server_url = "marquisogre.xyz/";  // ENTER WEBSITE URL ALONG WITH A TRAILING SLASH
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "PqPSmwta6pCRSvUSu2LG737WeTnxYK";
+$db_name = "wallet";
+
+$rpc_host = "127.0.0.1";
+$rpc_port = "9191";
+$rpc_user = "rpc_terracash";
+$rpc_pass = "Cp68nBkCAADKkskaKSskaDKdmSYLtLJ";
+
+$fullname = "TerraCash"; //Website Title (Do Not include 'wallet')
+$short = "TRCH"; //Coin Short (BTC)
+$blockchain_tx_url = "http://80.78.254.169:3001/tx/"; //Blockchain Url
+$support = "support@terracash.io"; //Your support eMail
+$hide_ids = array(1); //Hide account from admin dashboard
+$donation_address = ""; //Donation Address
+
+$reserve = "0"; //This fee acts as a reserve. The users balance will display as the balance in the daemon minus the reserve. We don't reccomend setting this more than the Fee the daemon charges.
+//Recaptcha
+$public = "6LcTAmYUAAAAAOAf0sp8TMQJNa2dsWduODD1WNlW";
+$secret = "6LcTAmYUAAAAAAvPrVpUGtLgJ7qF7-627OJAZ5Mx";
+
+?>
